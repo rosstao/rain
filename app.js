34f3da3e34568c1599d83ee691fa9807ce0db953
xuyuxing <kwin.xu@semioe.com>
2016-07-13 18:13:57 +0800

    更改文件夹名称

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express()
 const http = require('http');
 const port = 8080;
 
-require('./frameWorkConfig/express')(app);
-require('./frameWorkConfig/routes')(app);   //设置访问路由
+require('./config/express')(app);
+require('./config/routes')(app);   //设置访问路由
 
 const server = http.createServer(app);
 server.listen(port, ()=> {
